3d541f0d0f80e75c6d526ac55dcec1143380ffc8
samkumar266 <sxp90460@ucmo.edu>
2025-06-21 17:11:45 -0500

    fix deploy issue: mergerd deploy

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const app = express()
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 const authRoute = require('./routes/auth-route')
 const bookRoute = require('./routes/booking-route')
 const CRUDroute = require('./routes/CRUD-route')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 mongoose.connect(
     "mongodb+srv://akshithaksh56:test123@plan-your-trip.7hsuik9.mongodb.net/appData",
@@ -19,7 +20,7 @@ mongoose.connect(
         }
     }
 )
-
+app.use(express.static(path.join(__dirname, '../web_app/dist/web_app')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -32,9 +33,10 @@ app.use('/book', bookRoute)
 app.use('/crud', CRUDroute)
 
 
-app.get('/', (req, res) => {
-    res.send("Hey World")
-})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../web_app/dist/web_app/index.html'));
+});
 
 app.listen(port, () => {
     console.log("server is connected : ",port)
